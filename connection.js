97cd09aa2f18f673bfebdccb205a16b0caada650
valmg87 <132629822+valmg87@users.noreply.github.com>
2023-10-01 22:20:09 -0500

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,9 @@
+// Importing the mongoose library
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/Thomas&Friends',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection
